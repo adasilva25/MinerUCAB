458cde386fb23e199dc3b807bf1e39b5ccb5c39a
adasilva25 <andreadasilvab@gmail.com>
2019-06-01 15:31:59 -0400

    Merging

--- minerucab-frontend/src/screens/Empleados/ConsultarEmpleados.js
+++ minerucab-frontend/src/screens/Empleados/ConsultarEmpleados.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import MenuBar from '../../components/MenuBar';
 import DataTable from '../../components/DataTable';
 import {Header} from '../../components/Header';
@@ -9,14 +8,6 @@ export default class ConsultarEmpleados extends React.Component {
     render(){
         return (
             <div>
-<<<<<<< HEAD
-                <MenuBar agregar={"/agregar_empleados"}/>
-                <DataTable 
-                    columns={'http://localhost:3000/column_names/test_table'} 
-                    data={'http://localhost:3000/users'
-                }
-                />
-=======
                 <Header />
                     <MenuBar agregar={"/agregar_empleados"}/>
                     <DataTable 
@@ -24,7 +15,6 @@ export default class ConsultarEmpleados extends React.Component {
                         data={'http://localhost:3000/users'}
                     />
                 <Footer />
->>>>>>> origin/Alba
             </div>
         )
     }
