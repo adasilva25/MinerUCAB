157fd8e327fa98521e1a45bb614805be989266f7
Diego Gutierrez <diegoalfonsogutierrez@gmail.com>
2019-07-01 07:33:23 -0400

    Mergin Andrea y alba 4

--- minerucab-frontend/src/screens/Explotaciones/ModificarExplotacion.js
+++ minerucab-frontend/src/screens/Explotaciones/ModificarExplotacion.js
@@ -12,7 +12,7 @@ import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Tab from 'react-bootstrap/Tab'
+import Tab from 'react-bootstrap/Tab' 
 import Tabs from 'react-bootstrap/Tabs'
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
